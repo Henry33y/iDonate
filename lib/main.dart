0775d0e42a53cd7298052af49f98417f45205b5e
Henry <henryasante336@gmail.com>
2025-04-16 13:08:00 +0000

    feat: integrate Firebase with Flutter, adding necessary configurations and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 // ignore_for_file: override_on_non_overriding_member
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'splash_screen.dart';  // Import the splash screen
+import 'splash_screen.dart'; // Import the splash screen
 // Updated import
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeProvider(),
